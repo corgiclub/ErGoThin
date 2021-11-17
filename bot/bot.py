28567d33fe0fdffac29e834d2760674e2027cdb7
Shimada666 <649940882@qq.com>
2021-11-17 14:12:40 +0800

    feat: add apscheduler plugin

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,11 @@
 import nonebot
 from nonebot.adapters.cqhttp import Bot as CQHTTPBot
-from nonebot.log import logger
 
 nonebot.init()
+
 driver = nonebot.get_driver()
 driver.register_adapter("cqhttp", CQHTTPBot)
 nonebot.load_plugins("src/plugins")
 
 if __name__ == "__main__":
-    nonebot.run(host='0.0.0.0', port=9081)
+    nonebot.run()
