c9e829b47f736b0015b696a9a6058faf3f3ddd84
Shimada666 <649940882@qq.com>
2021-11-19 12:52:35 +0800

    打卡时间13,17

--- bot/src/plugins/notice/__init__.py
+++ bot/src/plugins/notice/__init__.py
@@ -5,7 +5,7 @@ from nonebot import require
 scheduler: BackgroundScheduler = require("nonebot_plugin_apscheduler").scheduler
 
 
-@scheduler.scheduled_job("cron", hour="10,17")
+@scheduler.scheduled_job("cron", hour="13,17")
 async def notice():
     bot = nonebot.get_bot()
     await bot.send_group_msg(group_id=590819255, message=f'打卡！')
